de3f5d8fd50549f5c3db821ffbd9c3380d041e5a
Ahamed Musthafa <amrs.tech@gmail.com>
2019-10-03 20:51:21 +0530

    Create hello_amrs-tech.py

---
+++ Python/hello_amrs-tech.py
@@ -0,0 +1,11 @@
+""" I love this hello <user> program for its simplicity """
+
+# Python 3.x
+
+print('Hello',input('Tell your name: '))
+
+"""
+Output be like - 
+Tell your name: <name_input>
+Hello <name_input>
+"""
