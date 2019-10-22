22af5f9f2eedf05bfa84a321e741ab553c5462d1
Jamie Ng <44282919+ratesmayapply@users.noreply.github.com>
2019-10-21 19:02:36 -0700

    Create Add_Subtract.py

---
+++ Python/Add_Subtract.py
@@ -0,0 +1,19 @@
+# This program can add or subtract to numbers
+
+def subtract(x, y):
+  return x - y
+
+def add(x, y):
+  return x + y
+
+def main():
+  print(add(2, 1))
+  print(subtract(7, 4))
+
+if __name__ == '__main__':
+  main()
+
+# Output:
+
+3
+3
