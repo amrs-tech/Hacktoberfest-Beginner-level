e627a800158182b05ab9a76250907956e69c2609
Kishan Kumar <kishankrs@github.com>
2019-10-28 16:21:06 +0530

    made program to find reverse of number in c

---
+++ C/reverse_of_integer_Kishan.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,rev=0;
+    printf("Enter the numbers you want to reverse");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+    rem=n%10;
+    rev=rev*10+rem;
+    n=n/10;
+    }
+    printf("reversed number is %d",rev);
+    return 0;
+}
