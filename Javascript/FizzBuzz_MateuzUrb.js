32274090c06790ef421f4f2d11feb83d237e0e86
Mateusz Urban <98m.urban@gmail.com>
2020-10-01 13:22:45 +0200

    create and update file

--- Javascript/FizzBuzz_MateuzUrb.js
+++ Javascript/FizzBuzz_MateuzUrb.js
@@ -1,5 +1,5 @@
-function fizzBuzz (x) {
-    for (let i = 1; i <= x; i++) {
+function fizzBuzz (num) {
+    for (let i = 1; i <= num; i++) {
         let output = '';
             if (i % 3 === 0) {output += "Fizz"}
             if (i % 5 === 0) {output += "Buzz"}
